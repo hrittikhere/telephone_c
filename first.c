c7db3c5bfb238dfa45a94a7859bf347357d030e2
Hrittik Roy <hrittikcom@gmail.com>
2021-07-10 17:08:26 +0530

    general refactor done

--- first.c
+++ first.c
@@ -24,9 +24,8 @@ like to make this project a better one.*/
 
 #include <stdio.h>
 #include <stdlib.h> //for exit
-#include <conio.h> //for getche() 
+#include <conio.h>	//for getche()
 #include <string.h> //for strcmp
-
 
 struct subscriber
 {
@@ -34,13 +33,15 @@ struct subscriber
 	char name[50];
 	float amount;
 } s;
+
+
+
 void addrecords();
 void listrecords();
 void modifyrecords();
 void deleterecords();
 void searchrecords();
 void payment();
-char get;
 int main()
 {
 	int phonenumber;
