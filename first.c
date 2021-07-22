c96661658eef7ef2620811ff72b880cfbac6e901
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2021-07-22 18:18:54 +0000

    Add readme

--- first.c
+++ first.c
@@ -1,27 +1,3 @@
-
-/*
-With this mini project in C Telecom Billing System, you can perform and manage
-billing operations like they do in Telecom companies. Here, you can add records
-with name, phone number and the amount of payment. You can view, modify, search
-and delete existing records. You can also make payment by providing your phone
-number instead of name. All data added or modified are recorded in a file. This
-project will teach you how to add, list, edit, search and delete records using file
-handling.
-The functions and features used in this project are listed below:
-
-
- Add new records: add new data into file with name, phone number and
-amount to be paid
- View list of records – lists all telephone billing records (as shown in
-screenshot 3)
- Modify records – edit the added records; name, phone number and/or
-amount of payment can be edited
- View payment – shows amount to be paid
- Search records – search for added/updated records in file
- Delete records – removes data permanently from file
-This project is limited to the functions aforementioned. You can add new features as you
-like to make this project a better one.*/
-
 #include <stdio.h>	// for file related
 #include <stdlib.h> //for exit
 #include <conio.h>	//for getche()
